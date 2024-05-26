54639bd06a22d18b761ded4062971080a4dbefcc
Daniel P Pflager <daniel.pflager@gmail.com>
2024-05-26 16:10:38 -0700

    Corrected spelling and added comment-question

--- frmmain.cpp
+++ frmmain.cpp
@@ -869,7 +869,7 @@ void frmMain::on_cmdFileSend_clicked() {
     ui->chkKeyboardControl->setChecked(false);
 
     if (!ui->chkTestMode->isChecked()) {
-        storeOffsets(); // Allready stored on check
+        storeOffsets(); // Already stored on check TODO:DPP: Stored on check of what?
     }
     storeParserState();
 
