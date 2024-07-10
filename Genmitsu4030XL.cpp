64ee5f534076a5b5f22f087b7bab9f80a71cff1c
Daniel P Pflager <daniel.pflager@gmail.com>
2024-07-09 17:06:46 -0700

    Added grblController class skeleton.

---
+++ Genmitsu4030XL.cpp
@@ -0,0 +1,5 @@
+//
+// Created by oy753c on 7/9/24.
+//
+
+#include "Genmitsu4030XL.h"
---
+++ Genmitsu4030XL.h
@@ -0,0 +1,16 @@
+//
+// Created by oy753c on 7/9/24.
+//
+
+#ifndef CANDLE_GENMITSU4030XL_H
+#define CANDLE_GENMITSU4030XL_H
+
+class Genmitsu4030XL {
+
+	// Find the limits of the carriage
+	public: void findCorners() {
+	}
+
+};
+
+#endif //CANDLE_GENMITSU4030XL_H
